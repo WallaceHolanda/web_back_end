f767a72411b4296e78dd9eb1ae7426fd36ff669a
Ubuntu <ubuntu@ip-172-31-28-152.ec2.internal>
2022-06-11 17:34:58 +0000

    Criando o Model de Cliente

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Clientes(models.Model):
+  nome = models.CharField(max_length=255)
+  cpf = models.CharField(max_length=15)
+  nascimento = models.CharField(max_length=11)
+  email = models.CharField(max_length=255)
+  senha = models.CharField(max_length=50)
 
 # Create your models here.
