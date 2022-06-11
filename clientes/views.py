2613804759ddbc9e795d216c4e90f9b6254aba6e
WallaceHolanda <wallace.holanda@hotmail.com>
2022-06-11 20:23:37 +0000

    Adicionando a Listagem de Clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Clientes
 
 def index(request):
-    template = loader.get_template('meuPrimeiro.html')
-    return HttpResponse(template.render())
+    listaClientes = Clientes.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+     'listaClientes':listaClientes,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
