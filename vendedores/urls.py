3f274ddf23f039a86b07666b076fc1126771bb0e
WallaceHolanda <wallace.holanda@hotmail.com>
2022-06-12 20:46:40 +0000

    Criando a tela de Adicionar Vendedor

--- vendedores/urls.py
+++ vendedores/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('adicionar/', views.adicionar, name='adicionar'),
+    path('adicionar/addvendedor/', views.addvendedor, name='addvendedor'),
 ]
--- vendedores/views.py
+++ vendedores/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect 
 from django.template import loader
+from django.urls import reverse
 from .models import Vendedores
 
 def index(request):
@@ -9,3 +11,20 @@ def index(request):
     'listaVendedores': listaVendedores,
   }
   return HttpResponse(template.render(context, request))
+
+def adicionar(request):
+  template = loader.get_template('adicionarVendedor.html')
+  return HttpResponse(template.render({}, request))
+
+def addvendedor(request):
+  vend_nome = request.POST['nome']
+  vend_documento = request.POST['documento']
+  vend_endereco = request.POST['endereco']
+  vend_telefone = request.POST['telefone']
+  vend_banco = request.POST['banco']
+  vend_conta = request.POST['conta']
+  vend_email = request.POST['email']
+  vend_senha = request.POST['senha']
+  vendedor = Vendedores(nome=vend_nome, documento=vend_documento, endereco=vend_endereco, telefone=vend_telefone, banco=vend_banco, conta=vend_conta, email=vend_email, senha=vend_senha)
+  vendedor.save()
+  return HttpResponseRedirect(reverse('index'))
