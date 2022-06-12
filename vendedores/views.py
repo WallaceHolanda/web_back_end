64b75c8d5e74a47a914dfd875ec1d1ea177eb3c6
WallaceHolanda <wallace.holanda@hotmail.com>
2022-06-12 19:41:26 +0000

    Adicionando a Listagem de Vendedores

--- vendedores/views.py
+++ vendedores/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Vendedores
 
 def index(request):
-  template = loader.get_template('meuPrimeiro.html')
-  return HttpResponse(template.render())
+  listaVendedores = Vendedores.objects.all().values()
+  template = loader.get_template('indexVendedores.html')
+  context = {
+    'listaVendedores': listaVendedores,
+  }
+  return HttpResponse(template.render(context, request))
