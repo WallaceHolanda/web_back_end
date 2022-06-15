c28f2cecef764800b6d6b079c3b4c1e555647045
WallaceHolanda <wallace.holanda@hotmail.com>
2022-06-15 00:14:30 +0000

    Criando o model de produtos

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Produtos(models.Model):
+  nome = models.CharField(max_length=100)
+  vendedor = models.CharField(max_length=255)
+  valor = models.CharField(max_length=20)
+  imagem = models.CharField(max_length=200)
