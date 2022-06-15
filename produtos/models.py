cf29cefc2eb6a82995345ec9e01f5895ac933c09
Wallace Holanda <wallace.holanda@hotmail.com>
2022-06-14 23:31:53 -0300

    Adicionando as imagens na pasta static

--- produtos/models.py
+++ produtos/models.py
@@ -3,4 +3,3 @@ class Produtos(models.Model):
   nome = models.CharField(max_length=100)
   vendedor = models.CharField(max_length=255)
   valor = models.CharField(max_length=20)
-  imagem = models.CharField(max_length=200)
