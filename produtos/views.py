f62eb31807e43a6cb1dbd3e940a11deacdd00b57
Wallace Holanda <wallace.holanda@hotmail.com>
2022-06-14 22:01:28 -0300

    Resolvendo o problema na listagem de produtos

--- produtos/views.py
+++ produtos/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Produtos
 
-def indexvendedor(request):
+def indexproduto(request):
   listaProdutos = Produtos.objects.all().values()
   template = loader.get_template('indexProdutos.html')
   context = {
