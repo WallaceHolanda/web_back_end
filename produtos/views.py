14c8addd5ce3a9911b2d5f114e55abed94e15daf
Wallace Holanda <wallace.holanda@hotmail.com>
2022-06-14 21:59:26 -0300

    Adicionando a Listagem de Produtos

--- produtos/views.py
+++ produtos/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Produtos
 
-def index(request):
-  template = loader.get_template('meuPrimeiro.html')
-  return HttpResponse(template.render())
+def indexvendedor(request):
+  listaProdutos = Produtos.objects.all().values()
+  template = loader.get_template('indexProdutos.html')
+  context = {
+    'listaProdutos': listaProdutos,
+  }
+  return HttpResponse(template.render(context, request))
